650d2bd1ebf5493b680fe5d883b091b428a046c8
Lakshmi Prasanna <prasanna.kandregula.lpk@gmail.com>
2024-09-03 11:56:19 -0400

    Selenium code - Sep3

---
+++ src/test/java/utility/BrowserDriver.java
@@ -0,0 +1,11 @@
+package utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class BrowserDriver {
+
+    public static WebDriver driver;
+
+
+}
